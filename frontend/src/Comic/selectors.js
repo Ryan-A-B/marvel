dc5675e00b2b9b1e1f979073865d223845a39c9a
Ryan <ryan.a.ballinger@gmail.com>
2017-10-08 09:30:42 +1000

    Style tweaks, removed unused code

--- frontend/src/Comic/selectors.js
+++ frontend/src/Comic/selectors.js
@@ -2,15 +2,3 @@ import {getAsync} from "../selectors.js";
 
 export const getComics = (state) => state.comics;
 export const getComic = (state, id) => getComics(state)[id];
-
-export const showRedirectModal = (state) => {
-    const async = getAsync(state);
-
-    const {viewComic} = async;
-
-    if (viewComic) {
-        return async[`comic-${viewComic}`] == Const.PENDING;
-    }
-
-    return false;
-};
